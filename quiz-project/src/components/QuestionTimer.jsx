16a86eab2dd6b01d48effab9d410667fc5f619b8
Kyle Pickard <kylepickard95@gmail.com>
2024-09-09 18:59:01 -0700

    Small Updates

--- quiz-project/src/components/QuestionTimer.jsx
+++ quiz-project/src/components/QuestionTimer.jsx
@@ -4,15 +4,23 @@ const QuestionTimer = ({ timeout, onTimeout }) => {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
-    setTimeout(() => {
+    // Set a timeout to call onTimeout after the specified timeout duration
+    const timeoutId = setTimeout(() => {
       onTimeout();
     }, timeout);
+
+    // Cleanup the timeout when the component unmounts or when the effect re-runs
+    return () => clearTimeout(timeoutId);
   }, [timeout, onTimeout]);
 
   useEffect(() => {
-    setInterval(() => {
+    // Set an interval to update the remaining time every 100ms
+    const intervalId = setInterval(() => {
       setRemainingTime((prevTime) => prevTime - 100);
     }, 100);
+
+    // Cleanup the interval when the component unmounts or when the effect re-runs
+    return () => clearInterval(intervalId);
   }, []);
 
   return <progress id="question-time" max={timeout} value={remainingTime} />;
--- quiz-project/src/components/QuizCard.jsx
+++ quiz-project/src/components/QuizCard.jsx
@@ -1,5 +1,3 @@
-import QuestionTimer from "./QuestionTimer";
-
 const QuizCard = ({
   quizQuestion,
   quizAnswers,
@@ -16,8 +14,6 @@ const QuizCard = ({
 
   return (
     <div className="flex flex-col m-auto px-8 py-5 w-[70%] rounded-lg bg-purple-800 justify-center">
-      <QuestionTimer timeout={10000} onTimeout={() => {}} />
-
       <h4 className="mb-5 text-2xl">{quizQuestion}</h4>
 
       {/* Displaying the correct answer if incorrect */}
@@ -32,16 +28,15 @@ const QuizCard = ({
       {quizAnswers.map((answer) => (
         <button
           key={answer.id}
-          className={`flex flex-col my-1 p-3 rounded-3xl items-center justify-center bg-purple-400 hover:bg-purple-600 ${
+          className={`flex flex-col my-1 p-3 rounded-3xl items-center justify-center ${
             currentPlayerAnswer !== undefined
-              ? answer.isCorrect === false
-                ? "bg-red-500 hover:bg-red-500"
-                : "bg-green-500 hover:bg-green-500"
-              : "bg-purple-400 hover:bg-purple-600 "
+              ? answer.isCorrect
+                ? "bg-green-500 hover:bg-green-500"
+                : "bg-red-500 hover:bg-red-500"
+              : "bg-purple-400 hover:bg-purple-600"
           }`}
           // Diable clicking once an answer has been selected
           disabled={currentPlayerAnswer !== undefined}
-          //
           // Updating the Player Data State
           onClick={() => updatePlayerData(answer, quizQuestion)}
         >
